271649e07ea0114e2c86f33f3dd47a861fa4dac3
Ocraniawan Patattan <ocraniawanpatattan@gmail.com>
2021-08-05 12:39:03 +0700

    firebase deployment

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome back Commander!')
-// })
-
 const flickrRoutes = require('./routes/flickr')
 
 app.use('/flickr', flickrRoutes)
